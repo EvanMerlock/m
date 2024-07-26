d076f48c4182e936fa85292cba915acee3af3a86
Evan Merlock <evan@merlock.dev>
2024-07-25 23:18:45 -0500

    feat: add initial files

--- bug-repros/otel-java-inst-11880/build.gradle.kts
+++ bug-repros/otel-java-inst-11880/build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.kafka:kafka-clients:3.7.1")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-kafka-clients-2.6:2.6.0-alpha")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ bug-repros/otel-java-inst-11880/src/main/java/dev/merlock/Main.java
@@ -0,0 +1,4 @@
+package dev.merlock;
+
+public class Main {
+}
