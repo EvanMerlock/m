a9c275dc3ea15ec1c0996bb490bd6572cae65a70
Evan Merlock <evan@merlock.dev>
2024-11-26 21:58:48 -0600

    fix: switch agent output naming to be more clear

--- blog-posts/java-agent/java/hwagent/src/main/java/dev/merlock/hwagent/HWAgent.java
+++ blog-posts/java-agent/java/hwagent/src/main/java/dev/merlock/hwagent/HWAgent.java
@@ -5,11 +5,11 @@ import java.lang.instrument.Instrumentation;
 public class HWAgent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("Hello from the Agent!");
+        System.out.println("Hello from the PreMain!");
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
-
+        System.out.println("Hello from the AgentMain!");
     }
 
 }
